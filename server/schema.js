a2e15a1e652bc3856852bb19a7247ebfe9632972
gotrecillo <sergio.panadero.perez@gmail.com>
2018-07-13 13:35:36 +0200

    add server mutations

--- server/schema.js
+++ server/schema.js
@@ -63,8 +63,63 @@ const Query = new GraphQLObjectType({
   }),
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'This is a root Mutation',
+  fields: () => ({
+    deleteArticle: {
+      type: articleType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(root, { id }) {
+        return Types.ObjectId.isValid(id)
+          ? db.Article.findByIdAndRemove(id)
+          : null;
+      },
+    },
+    createArticle: {
+      type: articleType,
+      args: {
+        author: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        excerpt: { type: GraphQLString },
+        published: { type: new GraphQLNonNull(GraphQLBoolean) },
+        tags: { type: new GraphQLList(GraphQLString) },
+        title: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(root, args) {
+        const article = {
+          excerpt: args.content.slice(0, 350),
+          ...args,
+        };
+
+        return db.Article.create(article);
+      },
+    },
+    updateArticle: {
+      type: articleType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        author: { type: GraphQLString },
+        content: { type: GraphQLString },
+        excerpt: { type: GraphQLString },
+        published: { type: GraphQLBoolean },
+        tags: { type: GraphQLString },
+        title: { type: GraphQLString },
+      },
+      resolve(root, { id, ...args }) {
+        return db.Article.findByIdAndUpdate(id, args, { new: true });
+      },
+    },
+  }),
+});
+
 const Schema = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
 
 export default Schema;
